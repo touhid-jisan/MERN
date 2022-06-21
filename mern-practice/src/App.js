be94a5de4d0a6a936f19429f5565649958c24d69
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2022-06-22 00:54:20 +0600

    stateManagement useState

--- mern-practice/src/App.js
+++ mern-practice/src/App.js
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => {
   const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
+    setCourseGoals(courseGoals.concat(newGoal));
     console.log(courseGoals);
   };
 
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: "id1", text: "Finish The course" },
     { id: "id2", text: "Finish The course 2" },
     { id: "id3", text: "Finish The course 3" },
     { id: "id4", text: "Finish The course 4" },
-  ];
+  ]);
 
   return (
     <div className="App">
