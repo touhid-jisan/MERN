01a8b16731eb9e3d2bbcd4e252eb1400a53c2275
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2022-06-21 23:07:31 +0600

    props parent to child

--- mern-practice/src/App.js
+++ mern-practice/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import GoalList from "./components/GoalList/GoalList";
 
 const App = () => {
-  return <div className="App">32323</div>;
+  const courseGoals = [
+    { id: "id1", text: "Finish The course" },
+    { id: "id2", text: "Finish The course 2" },
+    { id: "id3", text: "Finish The course 3" },
+    { id: "id4", text: "Finish The course 4" },
+  ];
+  return (
+    <div className="App">
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
 
 export default App;
---
+++ mern-practice/src/components/GoalList/GoalList.js
@@ -0,0 +1,15 @@
+import "./GoalList.css";
+
+const GoalList = (props) => {
+  return (
+    <div>
+      <ul className="goal-list">
+        {props.goals.map((goal) => {
+          return <li key={goal.id}>{goal.text}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default GoalList;
