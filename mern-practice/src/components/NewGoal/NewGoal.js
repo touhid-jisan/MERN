acd9eb5351ff74e37addbbb1674188d21c30e5ff
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2022-06-22 23:46:27 +0600

    fetching user input-two way binding

--- mern-practice/src/components/NewGoal/NewGoal.js
+++ mern-practice/src/components/NewGoal/NewGoal.js
@@ -1,19 +1,27 @@
 import "./NewGoal.css";
+import { useState } from "react";
 
 const NewGoal = (props) => {
+  const [enteredText, setEnteredText] = useState("");
+
   const addGoalHandler = (event) => {
     event.preventDefault();
 
     const newGoal = {
-      id: "1",
-      text: "Hello world",
+      id: Math.random(),
+      text: enteredText,
     };
+    setEnteredText("");
     props.onAddGoal(newGoal);
+  };
+
+  const textChangeHandler = (event) => {
+    setEnteredText(event.target.value);
   };
   return (
     <div>
       <form className="new-goal" onSubmit={addGoalHandler}>
-        <input type="text" />
+        <input type="text" value={enteredText} onChange={textChangeHandler} />
         <button type="submit">Add Goal</button>
       </form>
     </div>
