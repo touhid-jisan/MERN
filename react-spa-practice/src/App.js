b1817659dfdba2b12227d3f75dc412a20ec31f8c
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2022-06-26 00:15:40 +0600

    BrowserRouter, Routes, Route

--- react-spa-practice/src/App.js
+++ react-spa-practice/src/App.js
@@ -1,4 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />}></Route>
+        <Route path="/places/new" element={<NewPlace />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 export default App;
